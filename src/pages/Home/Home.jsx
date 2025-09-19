ffd7b857bd91cf64373a1b9e6e93266ca56bd8cb
gilmerperez <gilmer2002@outlook.com>
2025-09-18 20:19:48 -0400

    Added home content

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import styles from "./Home.module.css";
+import { Link } from "react-router-dom";
 
 function Home() {
   // * Set page title
@@ -10,7 +11,29 @@ function Home() {
   return (
     <>
       <main>
-        <div className={styles.homeContainer}>home</div>
+        <div className={styles.homeContainer}>
+          {/* Hero section */}
+          <section className={styles.heroSection}>
+            {/* Greeting */}
+            <h2 className={styles.greeting}>Hi, I'm Gilmer</h2>
+            {/* Main title */}
+            <h1 className={styles.mainTitle}>
+              FRONT-END
+              <br />
+              WEB DEVELOPER.
+            </h1>
+            {/* Description */}
+            <p className={styles.description}>
+              A passionate <strong>Front-End Web Developer</strong> and <strong>UI/UX Enthusiast</strong> with{" "}
+              <strong>Full-Stack Knowledge</strong>, specialized in building stunning pixel-perfect interactive websites
+              and applications. Passionate about creating engaging & dynamic web experiences.
+            </p>
+            {/* CTA button */}
+            <Link to="/about" className={styles.ctaButton}>
+              LEARN MORE
+            </Link>
+          </section>
+        </div>
       </main>
     </>
   );
