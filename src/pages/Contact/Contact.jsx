509077fb4c1b07f3a902093b9266c1dca3caf203
gilmerperez <gilmer2002@outlook.com>
2025-09-18 20:47:06 -0400

    Added contact page content with email and phone client

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -7,10 +7,40 @@ function Contact() {
     document.title = "Gilmer Perez | Contact";
   }, []);
 
+  // * Email handler
+  const handleEmailClick = () => {
+    window.location.href = "mailto:gilmer2002@outlook.com";
+  };
+
+  // * Phone handler
+  const handlePhoneClick = () => {
+    window.location.href = "tel:4073501805";
+  };
+
   return (
     <>
       <main>
-        <div className={styles.contactContainer}>contact</div>
+        <div className={styles.contactContainer}>
+          {/* Contact section */}
+          <section className={styles.contactSection}>
+            {/* Heading */}
+            <h1 className={styles.contactHeading}>GET IN TOUCH</h1>
+            {/* Description` */}
+            <p className={styles.contactDescription}>
+              Whether you are starting a project, have business inquiries or just want to say hi, I'm always open so
+              feel free to reach out and I will get back to you as soon as possible.
+            </p>
+            {/* CTA buttons */}
+            <div className={styles.buttonContainer}>
+              <button onClick={handleEmailClick} className={styles.contactButton} aria-label="Email me">
+                EMAIL ME
+              </button>
+              <button onClick={handlePhoneClick} className={styles.contactButton} aria-label="Call me">
+                CALL ME
+              </button>
+            </div>
+          </section>
+        </div>
       </main>
     </>
   );
