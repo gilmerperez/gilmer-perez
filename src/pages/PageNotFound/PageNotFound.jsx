baf3133df01e20574605a42871d53a5041d73823
gilmerperez <gilmer2002@outlook.com>
2025-09-18 23:46:28 -0400

    Added page not found page

--- src/pages/PageNotFound/PageNotFound.jsx
+++ src/pages/PageNotFound/PageNotFound.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import styles from "./PageNotFound.module.css";
+import { useNavigate } from "react-router-dom";
 
 function PageNotFound() {
   // * Set page title
@@ -7,10 +8,24 @@ function PageNotFound() {
     document.title = "Gilmer Perez | Page Not Found";
   }, []);
 
+  const navigate = useNavigate();
+  const handleBackToHome = () => {
+    navigate("/");
+  };
+
   return (
     <>
       <main>
-        <div className={styles.pageNotFoundContainer}>page not found</div>
+        <div className={styles.pageNotFoundContainer}>
+          {/* Title */}
+          <h1 className={styles.title}>PAGE NOT FOUND</h1>
+          {/* Description */}
+          <p className={styles.description}>Sorry, the page you are looking for does not exist</p>
+          {/* Back button */}
+          <button className={styles.backButton} onClick={handleBackToHome}>
+            BACK TO HOME PAGE
+          </button>
+        </div>
       </main>
     </>
   );
